2c2d714913076cfaeb564de839bd1e2ca69c99e2
warrenbrown <warren@katapultmedia.com>
2018-06-11 07:08:40 -0500

    added user auth to frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { Angular2TokenService } from 'angular2-token';
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -12,6 +15,10 @@ import { HomeComponent } from './home/home.component';
 import { PostDetailComponent } from './post-detail/post-detail.component';
 import { PostCreateComponent } from './post-create/post-create.component';
 import { PostUpdateComponent } from './post-update/post-update.component';
+import { AuthDialogComponent } from './auth-dialog/auth-dialog.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
 
 
 @NgModule({
@@ -21,15 +28,25 @@ import { PostUpdateComponent } from './post-update/post-update.component';
     HomeComponent,
     PostDetailComponent,
     PostCreateComponent,
-    PostUpdateComponent
+    PostUpdateComponent,
+    AuthDialogComponent,
+    ToolbarComponent,
+    LoginFormComponent,
+    RegisterFormComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
+    HttpModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    MaterializeModule
   ],
-  providers: [PostService],
+  providers: [
+    PostService,
+    Angular2TokenService,
+    HttpClientModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
